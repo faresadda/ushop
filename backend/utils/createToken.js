a87b82a5401ab7f9a183de888165b09e59282dd3
faresadda <faresadda2001off@gmail.com>
2025-06-01 20:49:09 +0100

    'commit'

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -5,7 +5,6 @@ const createToken = async (payload) =>{
   const token = await jwt.sign(
     payload,
     process.env.JWT_SECRET_KEY, 
-    {expiresIn: process.env.JWT_SECRET_KEY}
     )
     return token
 }
