f325cb1da378e1174acc849d64a810e83da8b1e5
faresadda <faresadda2001off@gmail.com>
2025-06-09 22:43:07 +0100

    'commit'

--- frontend/src/routes/admin.jsx
+++ frontend/src/routes/admin.jsx
@@ -4,7 +4,7 @@ import NotFound from "../components/NotFound";
 import AdminLayout from "../pages/admin/AdminLayout";
 import AdminProfile from "../pages/admin/AdminProfile";
 import AdminProducts from "../pages/admin/AdminProducts";
-import AddProduct from "../pages/admin/AddEditProduct";
+import AddEditProduct from "../pages/admin/AddEditProduct";
 import Users from "../pages/admin/Users";
 import Admins from "../pages/admin/Admins";
 
@@ -22,8 +22,8 @@ export default function Admin() {
         <Route index element={<Dashboard />} />
         <Route path="profile" element={<AdminProfile />} />
         <Route path="products" element={<AdminProducts />} />
-        <Route path="addproduct" element={<AddProduct />} />
-        <Route path="updateproduct/:id" element={<AddProduct />} />
+        <Route path="addproduct" element={<AddEditProduct />} />
+        <Route path="updateproduct/:id" element={<AddEditProduct />} />
         <Route path="users" element={<Users />} />
         <Route path="admins" element={<Admins />} />
         <Route path="*" element={<NotFound />} />
