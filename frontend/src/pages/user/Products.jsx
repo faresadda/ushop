2c71d4974c41f9fe59df1b0f66ffed86e426a623
faresadda <faresadda2001off@gmail.com>
2025-06-10 01:15:34 +0100

    'commit'

--- frontend/src/pages/user/Products.jsx
+++ frontend/src/pages/user/Products.jsx
@@ -29,8 +29,8 @@ export default function Products({ style }) {
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
   const currentProducts =products?.slice(indexOfFirstProduct, indexOfLastProduct) || [];
-  const totalPages = Math.ceil((products.length || 0) / productsPerPage);
-  const totalProducts = products.length || 0;
+  const totalPages = Math.ceil((products?.length || 0) / productsPerPage);
+  const totalProducts = products?.length || 0;
 
   useEffect(() => {
     setCurrentPage(1);
